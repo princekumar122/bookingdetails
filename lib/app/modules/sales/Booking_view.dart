4be55e878f2a822cfa1e551aa7c45000d47e7a04
soniprincegithub <prince22january@gmail.com>
2022-05-23 18:55:22 +0530

    accepted

--- lib/app/modules/sales/Booking_view.dart
+++ lib/app/modules/sales/Booking_view.dart
@@ -11,136 +11,124 @@ import 'history_bookings/history_view.dart';
 
 class BookingSection extends StatelessWidget {
   String tab = Get.parameters['tab']!;
-  
- 
+
   BookingSection({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context)
- {
+  Widget build(BuildContext context) {
     return Scaffold(
         body: Column(
-       // mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Padding(
+      // mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Padding(
           padding: const EdgeInsets.only(
-          top: 35,
+            top: 35,
           ),
           child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-         Padding(
-           padding: const EdgeInsets.only(left: 20),
-           child: ConstrainedBox(constraints: BoxConstraints(maxWidth: MediaQuery.of(context).size.width*0.2 ,
-           maxHeight:  MediaQuery.of(context).size.height*0.09 ),
-             child: buttons(AppStrings.ACCEPTED, context,  "Requested")),
-         ),
-        
-        Padding(
-          padding: const EdgeInsets.only(
-          left: 10,
-          ),
-          child: ConstrainedBox(constraints: BoxConstraints(maxWidth: MediaQuery.of(context).size.width*0.2,
-          maxHeight:  MediaQuery.of(context).size.height*0.09  ),
-          child: buttons(AppStrings.ACCEPTED,
-              context, "Accepted"),
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(left: 20),
+                child: ConstrainedBox(
+                    constraints: BoxConstraints(
+                        maxWidth: MediaQuery.of(context).size.width * 0.2,
+                        maxHeight: MediaQuery.of(context).size.height * 0.09),
+                    child: buttons(AppStrings.ACCEPTED, context, "Requested")),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(
+                  left: 10,
+                ),
+                child: ConstrainedBox(
+                  constraints: BoxConstraints(
+                      maxWidth: MediaQuery.of(context).size.width * 0.2,
+                      maxHeight: MediaQuery.of(context).size.height * 0.09),
+                  child: buttons(AppStrings.ACCEPTED, context, "Accepted"),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(
+                  left: 10,
+                ),
+                child: ConstrainedBox(
+                    constraints: BoxConstraints(
+                        maxWidth: MediaQuery.of(context).size.width * 0.2,
+                        maxHeight: MediaQuery.of(context).size.height * 0.09),
+                    child: buttons(
+                        AppStrings.HISTORY_BOOKING, context, "History")),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(
+                  left: 10,
+                ),
+                child: ConstrainedBox(
+                    constraints: BoxConstraints(
+                        maxWidth: MediaQuery.of(context).size.width * 0.2,
+                        maxHeight: MediaQuery.of(context).size.height * 0.09),
+                    child: buttons(AppStrings.ACCEPTED, context, "Confirm")),
+              ),
+            ],
           ),
         ),
-        Padding(
-          padding: const EdgeInsets.only(
-          left: 10,
+        Expanded(
+          child: Container(
+            //  constraints: BoxConstraints.expand(),
+            alignment: Alignment.bottomCenter,
+            margin: EdgeInsets.only(top: Get.height * 0.13),
+            height: Get.height * 0.9,
+            width: Get.width * 0.8,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(25),
+              color: Get.isDarkMode ? Colors.white54 : Colors.black54,
+              border: Border.all(
+                  color: Get.isDarkMode ? Colors.white54 : Colors.black54),
+            ),
+            child: tab == AppStrings.HISTORY_BOOKING
+                ? HistoryBookings()
+                : Accepted(),
           ),
-          child:
-            ConstrainedBox(constraints: BoxConstraints(maxWidth: MediaQuery.of(context).size.width*0.2,
-            maxHeight:  MediaQuery.of(context).size.height*0.09  ),
-              child: buttons(AppStrings.HISTORY_BOOKING,context, "History")),
-        ),
-        Padding(
-          padding: const EdgeInsets.only(
-          left: 10,
-          ),
-          child:
-            ConstrainedBox(constraints: BoxConstraints(maxWidth: MediaQuery.of(context).size.width*0.2,
-            maxHeight:  MediaQuery.of(context).size.height*0.09 ),
-              child: buttons(AppStrings.ACCEPTED,context,  "Confirm")),
-        ),
-          ],
-          ),
-          ),
-          Expanded(
-            child: Container(
-      //  constraints: BoxConstraints.expand(),
-        alignment: Alignment.bottomCenter,
-          margin: EdgeInsets.only(top: Get.height * 0.13),
-          height: Get.height * 0.9,
-          width: Get.width * 0.8,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(25),
-            color: Get.isDarkMode ? Colors.white54 : Colors.black54,
-            
-            border: Border.all(
-                color: Get.isDarkMode ? Colors.white54 : Colors.black54),
-          ),
-        child: tab == AppStrings.HISTORY_BOOKING? HistoryBookings():Accepted(),
-         
-         
-         
-        ),
-          ) ],
-            
+        )
+      ],
     ));
   }
 
   // BOOKING BUTTONS
 
-  Widget buttons(String navigationTab, BuildContext context,  var title) {
+  Widget buttons(String navigationTab, BuildContext context, var title) {
     return ListTile(
       selected: tab == navigationTab,
       leading: ElevatedButton(
           onPressed: () {
-            Get.toNamed('/home/${AppStrings.SALES}/booking-view/${navigationTab}');
+            Get.toNamed(
+                '/home/${AppStrings.SALES}/booking-view/${navigationTab}');
           },
-         style: ButtonStyle(
-            backgroundColor: MaterialStateProperty.resolveWith<Color>(
-                (Set<MaterialState> states) {
-              if (states.contains(MaterialState.pressed)) {
-                return Colors.grey;
-              }
+          style: ButtonStyle(
+              backgroundColor: MaterialStateProperty.resolveWith<Color>(
+                  (Set<MaterialState> states) {
+                if (states.contains(MaterialState.pressed)) {
+                  return Colors.grey;
+                }
 
-              return 
-               Colors.white  ;
-            
-            }),
-            shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(18.0),
-                    side: BorderSide(
-                      color: Get.isDarkMode ? Colors.black : Colors.white,
-                    )))),
-       
+                return Colors.white;
+              }),
+              shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                  RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(18.0),
+                      side: BorderSide(
+                        color: Get.isDarkMode ? Colors.black : Colors.white,
+                      )))),
           child: Padding(
             padding: const EdgeInsets.all(4.0),
             child: Text(
               title,
               style: TextStyle(
-                fontSize: 17,
-                fontWeight: FontWeight.normal,
-               color:Colors.black
-              ),
+                  fontSize: 17,
+                  fontWeight: FontWeight.normal,
+                  color: Colors.black),
             ),
           )),
     );
   }
+
+  Accepted() {}
 }
-
-
-
-
-
-
-
-
-
-
-
-
--- lib/app/modules/sales/accepted_bookings/accepted_view.dart
+++ lib/app/modules/sales/accepted_bookings/accepted_view.dart
@@ -1,14 +1,43 @@
-import 'package:flutter/cupertino.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
-class Accepted extends StatelessWidget {
-  const Accepted({ Key? key }) : super(key: key);
+class Accepted extends StatefulWidget {
+  Accepted({Key? key}) : super(key: key);
 
+  @override
+  State<Accepted> createState() => _AcceptedState();
+}
+
+class _AcceptedState extends State<Accepted> {
+  Map<String, dynamic>? info;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text("hi",style: TextStyle(fontSize: 30),)),
-      
+      body: StreamBuilder<QuerySnapshot>(
+          stream: FirebaseFirestore.instance
+              .collection("/assets_folder/puja_bookings_folder/puja_bookings")
+              .where("puja_status", isEqualTo: "accepted")
+              .snapshots(),
+          builder: ((context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return Center(child: CircularProgressIndicator());
+            }
+            if (snapshot.hasData) {
+              return hello(snapshot);
+            } else {
+              return Text("booking_id");
+            }
+          })),
     );
   }
+}
+
+hello(AsyncSnapshot<QuerySnapshot> snapshot) {
+  return ListView.builder(
+      itemCount: snapshot.data?.docs.length,
+      itemBuilder: (context, index) {
+        final rbookingMap =
+            snapshot.data?.docs[index].data() as Map<String, dynamic>;
+        return ListTile(title: Text(rbookingMap['user_uid']));
+      });
 }
